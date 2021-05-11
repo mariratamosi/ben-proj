340a20a2b39b55442263c5db46cd65d612712868
marira <rongrazzo@gmail.com>
2021-05-11 20:09:51 +0600

    express server installed

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
+import express from "express";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   orm.getMigrator().up();
   console.log("yee2");
-  const post = orm.em.create(Post, { title: "first post3" });
-  await orm.em.persistAndFlush(post);
 
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  const app = express();
+  app.get("/", (_, res) => {
+    res.send("hello");
+  });
+
+  app.listen(4000, () => {
+    console.log("http://localhost:4000");
+  });
+  // const post = orm.em.create(Post, { title: "first post3" });
+  // await orm.em.persistAndFlush(post);
+
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
 };
 
 console.log("hello oear");
