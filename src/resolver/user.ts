318cbaceac04721fa4d46799bee1a690c9a59242
marira <rongrazzo@gmail.com>
2021-05-13 00:11:56 +0600

    createUser:: register:: done

--- src/resolver/user.ts
+++ src/resolver/user.ts
@@ -1,6 +1,16 @@
 import { Post } from "../entities/Post";
 import { User } from "../entities/User";
-import { Resolver, Arg, Field, InputType, Mutation, Ctx } from "type-graphql";
+import {
+  Resolver,
+  Arg,
+  Field,
+  InputType,
+  Mutation,
+  Ctx,
+  Query,
+} from "type-graphql";
+import argon2 from "argon2";
+import { MyContext } from "../types";
 
 @InputType()
 class UsernamePasswordInput {
@@ -13,12 +23,21 @@ class UsernamePasswordInput {
 
 @Resolver()
 export class UserResolver {
-  @Mutation(() => Post) //graphql -type
-  async createUser(
+  @Query(() => [User])
+  users(@Ctx() { em }: MyContext): Promise<User[]> {
+    return em.find(User, {});
+  }
+
+  @Mutation(() => User) //graphql -type
+  async register(
     @Arg("options") options: UsernamePasswordInput,
     @Ctx() { em }: MyContext
-  ): Promise<Post | null> {
-    const user = em.create(User, { username: options.username });
+  ): Promise<User | null> {
+    const hashedPassword = await argon2.hash(options.password);
+    const user = em.create(User, {
+      username: options.username,
+      password: hashedPassword,
+    });
     await em.persistAndFlush(user);
 
     return user;
