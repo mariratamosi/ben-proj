8d1c30457afdaaf6d9926511298781b9b1d1ba04
marira <rongrazzo@gmail.com>
2021-05-13 00:03:12 +0600

    createUser:: wip

--- src/resolver/user.ts
+++ src/resolver/user.ts
@@ -1,15 +1,26 @@
-import { Resolver, Query } from "type-graphql";
+import { Post } from "../entities/Post";
+import { User } from "../entities/User";
+import { Resolver, Arg, Field, InputType, Mutation, Ctx } from "type-graphql";
+
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string;
+
+  @Field()
+  password: string;
+}
 
 @Resolver()
 export class UserResolver {
   @Mutation(() => Post) //graphql -type
-  async createPost(
-    @Arg("title") title: string,
+  async createUser(
+    @Arg("options") options: UsernamePasswordInput,
     @Ctx() { em }: MyContext
   ): Promise<Post | null> {
-    const post = em.create(Post, { title });
-    await em.persistAndFlush(post);
+    const user = em.create(User, { username: options.username });
+    await em.persistAndFlush(user);
 
-    return post;
+    return user;
   }
 }
