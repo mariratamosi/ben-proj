b7cdb152137728334e1889d8834bdfd91ea8eed1
marira <rongrazzo@gmail.com>
2021-05-12 23:12:20 +0600

    post retrieved by ID

--- src/resolver/post.ts
+++ src/resolver/post.ts
@@ -1,11 +1,20 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "../types";
-import { Resolver, Query, Ctx } from "type-graphql";
+import { Resolver, Query, Ctx, Arg, Int } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
   posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true }) //graphql -type
+  post(
+    @Arg("identity", () => Int) _id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    //ts type
+    return em.findOne(Post, { _id });
   }
 }
