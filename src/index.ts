eaecf7c27119df50613ed2dd805d2e10dcf0c5eb
marira <rongrazzo@gmail.com>
2021-06-29 02:38:39 +0600

    redis added

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,54 @@ import { HelloResolver } from "./resolver/hello";
 import { PostResolver } from "./resolver/post";
 import { UserResolver } from "./resolver/user";
 
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { MyContext } from "./types";
+
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   orm.getMigrator().up();
 
   const app = express();
 
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  redisClient.on("error", function (err) {
+    console.log("Could not establish a connection with redis. " + err);
+  });
+
+  redisClient.on("connect", function (err) {
+    console.log("Connected to redis successfully", err);
+  });
+
+  //order matters
+  app.use(
+    session({
+      name: "qid", //random name
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365,
+        httpOnly: true, //won;t work in browser
+        secure: false, //production http hole remove it
+        sameSite: "lax",
+      },
+      saveUninitialized: false,
+      secret: "1234",
+      resave: false, //don't continue to ping redis
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em: orm.em }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
   });
 
   apolloServer.applyMiddleware({ app });
--- src/resolver/user.ts
+++ src/resolver/user.ts
@@ -1,4 +1,3 @@
-import { Post } from "../entities/Post";
 import { User } from "../entities/User";
 import {
   Resolver,
@@ -10,6 +9,7 @@ import {
   Query,
   ObjectType,
 } from "type-graphql";
+
 import argon2 from "argon2";
 import { MyContext } from "../types";
 
@@ -42,6 +42,17 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { req, em }: MyContext) {
+    if (!req.session.userId) {
+      return null;
+    }
+
+    const user = await em.findOne(User, { id: req.session.userId });
+
+    return user;
+  }
+
   @Query(() => [User])
   users(@Ctx() { em }: MyContext): Promise<User[]> {
     return em.find(User, {});
@@ -51,9 +62,8 @@ export class UserResolver {
   async register(
     @Arg("options") options: UsernamePasswordInput,
     @Ctx() { em }: MyContext
-  ): Promise<UserResponse > {
-
-    if (options.username.length<3){
+  ): Promise<UserResponse> {
+    if (options.username.length < 3) {
       return {
         errors: [
           {
@@ -61,11 +71,10 @@ export class UserResolver {
             message: "Length should be greater than 3",
           },
         ],
-
-      }
+      };
     }
 
-    if (options.password.length<3){
+    if (options.password.length < 3) {
       return {
         errors: [
           {
@@ -73,7 +82,7 @@ export class UserResolver {
             message: "Length should be greater than 3",
           },
         ],
-      }
+      };
     }
 
     const hashedPassword = await argon2.hash(options.password);
@@ -81,13 +90,12 @@ export class UserResolver {
       username: options.username,
       password: hashedPassword,
     });
-    try{
-    await em.persistAndFlush(user);
-    }
-    catch(err){
-      console.log(err.code)
+    try {
+      await em.persistAndFlush(user);
+    } catch (err) {
+      console.log(err.code);
 
-      if (err.code==="23505"){
+      if (err.code === "23505") {
         return {
           errors: [
             {
@@ -95,18 +103,18 @@ export class UserResolver {
               message: "Already exist",
             },
           ],
-        }
+        };
       }
     }
     return {
-      user
-    }
+      user,
+    };
   }
 
   @Mutation(() => UserResponse) //graphql -type
   async login(
     @Arg("options") options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse | null> {
     const user = await em.findOne(User, { username: options.username });
 
@@ -133,8 +141,11 @@ export class UserResolver {
         ],
       };
     }
+
+    req.session.userId = user.id;
+
     return {
-      user: user
-    }
-  
+      user: user,
+    };
+  }
 }
